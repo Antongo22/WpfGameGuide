ffca3b71771c09889bc8945d24832cc327d0ac2b
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2026-02-07 15:16:17 +0300

    улучшен визуал

---
+++ WpfGameGuide/Chapters/CheatSheets/CheatSheetPage.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace WpfGameGuide.Chapters.CheatSheets
+{
+    /// <summary>
+    /// Страница со шпаргалками по WPF элементам
+    /// </summary>
+    public partial class CheatSheetPage : Page
+    {
+        public CheatSheetPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
